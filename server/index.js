6384685d17663310e662113d36dd6ecab5707971
Ayush-Pandit <panditayush2703@gmail.com>
2024-09-27 12:29:45 +0530

    server-js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,17 @@
-const express=require("express")
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const path = require("path");
+const port = 3000;
 
-const app=express();
+const app = express();
 
-console.log("")
+app.get("/", (req, res) => {
+  res.send.json({
+    message: "Hello World",
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`Server is listening is ${port}...`);
+});
